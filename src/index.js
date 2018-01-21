470a15f1382fb5fce327631b67787bf737f2055d
Graham Barber <grahammbarber@gmail.com>
2018-01-20 18:30:32 -0800

    feat: add hyperapp-electron boilerplate code

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { app } = require('hyperapp')
+const { main, h1, div, button } = require('@hyperapp/html')
+
+const devtools = require('hyperapp-redux-devtools')
+
+const state = {
+  count: 0
+}
+
+const actions = {
+  reset: () => ({ count: 0 }),
+  sum: data => ({ count }) => ({ count: count + data })
+}
+
+const view = (state, actions) =>
+  main([
+    h1(state.count),
+    div([
+      button({ onclick: e => actions.sum(-1) }, 'Sub'),
+      button({ onclick: e => actions.reset() }, 'Reset'),
+      button({ onclick: e => actions.sum(1) }, 'Add')
+    ])
+  ])
+
+devtools(app)(state, actions, view, document.body)
