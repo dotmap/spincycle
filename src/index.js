3b09b9dd1145379f19d7aab48f358c04bf5eb896
Graham Barber <grahammbarber@gmail.com>
2018-01-21 00:03:02 -0800

    feat: fetch and list issues from a repository

--- src/index.js
+++ src/index.js
@@ -1,25 +1,57 @@
+const { get } = require('axios')
 const { app } = require('hyperapp')
-const { main, h1, div, button } = require('@hyperapp/html')
+const { main, h1, div, button, input, ul, li } = require('@hyperapp/html')
 
 const devtools = require('hyperapp-redux-devtools')
 
 const state = {
-  count: 0
+  user: '',
+  name: '',
+  issues: []
 }
 
 const actions = {
-  reset: () => ({ count: 0 }),
-  sum: data => ({ count }) => ({ count: count + data })
+  getIssues: ({ user, name }) => async (state, actions) => {
+    actions.setIssues(await get(`https://api.github.com/repos/${user}/${name}/issues`)
+      .then(issues => (issues.data.map(({number, title}, index) => ({number, title, active: index === 0})))))
+  },
+  setIssues: (issues) => ({issues}),
+  setUser: (user) => ({user}),
+  setName: (name) => ({name}),
+  setActive: (direction) => ({issues}) => {
+    const current = issues.findIndex(({active}) => (active === true))
+    issues[current].active = false
+    if (current + direction >= issues.length) issues[0].active = true
+    else if (current + direction < 0) issues[issues.length - 1].active = true
+    else issues[current + direction].active = true
+    return { issues }
+  }
 }
 
-const view = (state, actions) =>
-  main([
-    h1(state.count),
-    div([
-      button({ onclick: e => actions.sum(-1) }, 'Sub'),
-      button({ onclick: e => actions.reset() }, 'Reset'),
-      button({ onclick: e => actions.sum(1) }, 'Add')
+const view = (state, actions) => main([
+  h1('Search for a repo...'),
+  div([
+    input({
+      type: 'text',
+      name: 'user',
+      oninput: ({target: {value}}) => actions.setUser(value),
+      placeholder: 'User...'
+    }),
+    input({
+      type: 'text',
+      name: 'name',
+      oninput: ({target: {value}}) => actions.setName(value),
+      placeholder: 'Repo...'
+    })
+  ]),
+  div([
+    button({ onclick: e => actions.getIssues({user: state.user, name: state.name}) }, 'Request')
+  ]),
+  div([
+    ul([
+      state.issues.map(({number, title, active}) => (li({class: active ? 'active' : ''}, `${number}: ${title}`)))
     ])
   ])
+])
 
 devtools(app)(state, actions, view, document.body)
